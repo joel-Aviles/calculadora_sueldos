7fbbb8daad0fe97a6e9e73095d76cb52a85742ed
Joel-Aviles <j_aviles.b@seg.guanajuato.gob.mx>
2024-11-06 09:21:41 -0600

    refactor: delete converter.py

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from funcs.converter import converter, check_valid_dir
 from openpyxl import Workbook
 from datetime import datetime as dt
+from unidecode import unidecode
 import pandas as pd
 import os
 import sys
+import re
+
+# FUNCIÓN PARA BR SI EXISTE UN DIRECTORIO
+def check_valid_dir(dirpath):
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+        return dirpath
+    else:
+        return dirpath
+
+# FUNCIÓN PARA TENER UN PATH VÁLIDO PARA PANDAS
+def valid_path(path):
+    return re.sub("\s*", "", re.sub('[&%"]', '', path.replace("\\", "/")).replace("'", ""), count=1)
+
+# FUNCIÓN PARA TENER NOMBRES DE COLUMNAS FÁCILES DE MANEJAR
+def validname(name):
+    return unidecode(''.join(filter(str.isalpha, name)).lower())
+
+# FUNCIÓN PRINCIPAL -> CONVERTIR ARCHIVOS A PANDA'S DATAFRAME
+def converter(filename, sheetname="Hoja1"):
+
+    dataframe = pd.DataFrame(pd.read_excel(valid_path(filename)))
+    dataframe.dropna(how="all", inplace=True)
+    dataframe.columns = [validname(column) for column in dataframe.columns]
+
+    return dataframe
 
 # Definir constantes
 LEY_CONCEPTS = ["01", "58", "59", "5G", "70", "74", "77", "IN", "P2", "R9", "RV", "SS"]
