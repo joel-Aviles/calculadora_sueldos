dcc007967982a298287ebd4dfac0e9277345c369
Joel-Aviles <j_aviles.b@seg.guanajuato.gob.mx>
2024-11-07 08:47:02 -0600

    feat: :sparkles: finish elemental endpoints
    implement FastAPI endpoints using original code and helpers

---
+++ api.py
@@ -0,0 +1,103 @@
+from fastapi import FastAPI, File, UploadFile, Form, HTTPException
+from fastapi.responses import FileResponse
+from caculadora import process_and_create_excel
+from helpers import is_valid_file_path, clean_old_files
+import logging
+import shutil
+import os
+
+app = FastAPI()
+
+ALLOWED_MIME_TYPES = ["application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
+
+def validate_file_type(file: UploadFile):
+    if file.content_type not in ALLOWED_MIME_TYPES:
+        logging.warning("validate_file_type: Tipo de archivo no permitido.")
+        raise HTTPException(status_code=415, detail="Tipo de archivo no permitido.")
+
+# Llamar a la función de limpieza cuando la aplicación inicie
+@app.on_event("startup")
+async def startup_event():
+    clean_old_files("C:\\caluladora\\pensiones")
+
+@app.get("/health")
+def health():
+    return {"health": "OK"}
+
+@app.post("/procesar")
+async def procesar(
+    process_type: str = Form(...),
+    discount_percent: float = Form(...),
+    money_formula: str = Form(...),
+    payment_period: int = Form(...),
+    file: UploadFile = File(...),
+):
+    
+    validate_file_type(file)
+
+    temp_file_path = f"temp_{file.filename}"
+    generated_file = None
+
+    try:
+        # Guardar archivo subido temporalmente
+        with open(temp_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        
+        # Procesar el archivo
+        generated_file = process_and_create_excel(
+            process_type,
+            discount_percent,
+            money_formula,
+            payment_period,
+            temp_file_path
+        )
+
+        # Verificación adicional
+        if generated_file and os.path.exists(generated_file):
+            logging.info(f"Endpoint: procesar, file: {generated_file}")
+            return {"file_path": generated_file}
+        else:
+            logging.error(f"status: 500, Error al generar el archivo procesado, Endpoint: procesar")
+            raise HTTPException(status_code=500, detail="Error al generar el archivo procesado, Endpoint: procesar")
+
+    except Exception as e:
+        logging.critical(f"status: 500, Endpoint: procesar, Error interno: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error interno: {str(e)}")
+    finally:
+        if os.path.exists(temp_file_path):
+            os.remove(temp_file_path)
+
+@app.get("/descargar")
+async def descargar(file_path: str = Form(...)):
+    # Validar la ruta del archivo
+    if not is_valid_file_path(file_path):
+        logging.warning(f"Ruta no válida: {file_path}, Endpoint: descargar")
+        raise HTTPException(status_code=406, detail="Ruta no válida")
+    
+    try:
+        # Comprobar si el archivo existe en la ruta proporcionada
+        if os.path.exists(file_path):
+            # Se podría agregar un log de acceso aquí si se requiere
+            logging.info(f"Endpoint: descargar, file: {file_path}")
+            return FileResponse(file_path, filename=os.path.basename(file_path))
+        else:
+            logging.warning(f"Endpoint: descargar, Archivo no encontrado: {file_path}")
+            raise HTTPException(status_code=404, detail="Archivo no encontrado.")
+    except Exception as e:
+        logging.critical(f"Endpoint: descargar, Error al intentar descargar el archivo: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error al intentar descargar el archivo: {str(e)}")
+
+LOG_FILE_PATH = "calculadora.log"
+@app.get("/descargar-log")
+async def descargar_log():
+    # Verificar si el archivo de log existe
+    if not os.path.exists(LOG_FILE_PATH):
+        logging.warning(f"Endpoint: descargar-log, Archivo de log no encontrado.")
+        raise HTTPException(status_code=404, detail="Archivo de log no encontrado.")
+    
+    # Enviar el archivo como respuesta para descargarlo
+    return FileResponse(
+        path=LOG_FILE_PATH, 
+        filename="calculadora.log", 
+        media_type='text/plain'
+    )
--- caculadora.py
+++ caculadora.py
@@ -7,7 +7,7 @@ import sys
 import re
 
 # FUNCIÓN PARA BR SI EXISTE UN DIRECTORIO
-def check_valid_dir(dirpath):
+def validate_dir(dirpath):
     if not os.path.exists(dirpath):
         os.makedirs(dirpath)
         return dirpath
@@ -15,19 +15,19 @@ def check_valid_dir(dirpath):
         return dirpath
 
 # FUNCIÓN PARA TENER UN PATH VÁLIDO PARA PANDAS
-def valid_path(path):
+def validate_path(path):
     return re.sub("\s*", "", re.sub('[&%"]', '', path.replace("\\", "/")).replace("'", ""), count=1)
 
 # FUNCIÓN PARA TENER NOMBRES DE COLUMNAS FÁCILES DE MANEJAR
-def validname(name):
+def validate_name(name):
     return unidecode(''.join(filter(str.isalpha, name)).lower())
 
 # FUNCIÓN PRINCIPAL -> CONVERTIR ARCHIVOS A PANDA'S DATAFRAME
-def converter(filename, sheetname="Hoja1"):
+def excel_2_dataframe(filename, sheetname="Hoja1"):
 
-    dataframe = pd.DataFrame(pd.read_excel(valid_path(filename)))
+    dataframe = pd.DataFrame(pd.read_excel(validate_path(filename)))
     dataframe.dropna(how="all", inplace=True)
-    dataframe.columns = [validname(column) for column in dataframe.columns]
+    dataframe.columns = [validate_name(column) for column in dataframe.columns]
 
     return dataframe
 
@@ -47,7 +47,7 @@ def get_resource_path(relative_path):
 def load_excel_file(filename):
     """Carga un archivo Excel desde la ruta correcta, usando get_resource_path."""
     file_path = get_resource_path(f'excel_files/{filename}')
-    df_file = converter(file_path)
+    df_file = excel_2_dataframe(file_path)
     return df_file
 
 def calculate_amounts(percep_ord, percep_extra, deductions, ley_deductions, sueldo, formula_type):
@@ -107,7 +107,9 @@ def prompt_user_input():
     
     if process_type == '1':
         discount_percent = int(input("¿Qué porcentaje se le va a descontar al trabajador? ")) * 0.01
+
         money_formula = input("¿Qué fórmula se usará?\n 1) Líquido\n 2) Neto\n 3) Solo percepciones\n 4) Percepciones ordinarias - deducciones de ley\n 5) Percepciones extraordinarias - deducciones de ley\n 6) Solo ordinarias\n 7) Solo extraordinarias\n 8) Percepciones 07\nIngrese el número de su elección (default: 1): ")
+
         payment_period = int(input("¿Cuántas quincenas se le va a cobrar? "))
     else:
         discount_percent = 1
@@ -118,10 +120,148 @@ def prompt_user_input():
     
     return process_type, discount_percent, money_formula, payment_period, file_name
 
-def main():
+def process_and_create_excel(process_type, discount_percent, money_formula, payment_period, file_name):
+    # 1) Leer archivo
+    df = excel_2_dataframe(file_name)
+
+    # 2) Leer percepciones y deducciones
+    percep = load_excel_file("percepciones.xlsx")
+    perord = df[(df["tipoconcepto"] == "Percepción") & (df["conceptosiapsep"].isin(percep["clave"]))]
+    perext = df[(df["tipoconcepto"] == "Percepción") & (~df["conceptosiapsep"].isin(percep["clave"]))]
+
+    deducs = load_excel_file("deducciones.xlsx")
+    deducs["concepto"] = deducs["concepto"].astype(str)
+    gended = df[(df["tipoconcepto"] == "Deducción") & (df["conceptosiapsep"].isin(deducs["concepto"]))]
+    outded = df[(df["tipoconcepto"] == "Deducción") & (~df["conceptosiapsep"].isin(deducs["concepto"]))]
+
+    # 4) Obtener datos del empleado
+    fstqnaproc = df["qnaproc"].min()
+    lstqnaproc = df["qnaproc"].max()
+    personal = load_excel_file("Personal.xlsx")
+    rfc, name = get_personal_data(df, personal)
+
+    # 5) Crear y escribir en archivo Excel
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Prueba"
+
+    ws["B2"] = "Datos del Empleado"
+    ws["B3"] = "Nombre"
+    ws["B4"] = "RFC"
+    ws["B5"] = "Qna Devengada"
+    ws["C3"] = name
+    ws["C4"] = rfc
+    ws["C5"] = lstqnaproc
+
+    # Percepciones Ordinarias
+    data = [
+        {
+            "concepto": concepto,
+            "descrip": percep.loc[percep["clave"] == concepto, "descripcion"].iloc[0],
+            "suma": perord[(perord["conceptosiapsep"] == concepto) & (perord["qnaproc"] == lstqnaproc)]["importe"].sum(),
+            "tipo": "Percepción"
+        }
+        for concepto in perord["conceptosiapsep"].unique()
+    ]
+    xindex, total_percep_ord = write_excel(ws, f"Percepciones Ordinarias {lstqnaproc}", data, 7, "Total Percepciones Ordinarias")
+
+    # Deducciones Ordinarias
+    data = [
+        {
+            "concepto": concepto,
+            "descrip": deducs.loc[deducs["concepto"] == concepto, "descripcion"].iloc[0],
+            "suma": gended[(gended["conceptosiapsep"] == concepto) & (gended["qnaproc"] == lstqnaproc)]["importe"].sum(),
+            "tipo": "Deducción"
+        }
+        for concepto in gended["conceptosiapsep"].unique()
+    ]
+    xindex, total_deduc_ord = write_excel(ws, f"Deducciones Ordinarias {lstqnaproc}", data, xindex, "Total Deducciones Ordinarias")
+
+    # Percepciones Extraordinarias
+    nocont = load_excel_file("PercepExtra_NoContarPensiones.xlsx")
+    data = [
+        {
+            "concepto": concepto,
+            "descrip": perext.loc[perext["conceptosiapsep"] == concepto, "descripciondeconcepto"].iloc[0],
+            "suma": perext[(perext["conceptosiapsep"] == concepto) & (perext["qnapago"] >= fstqnaproc)]["importe"].sum(),
+            "tipo": "Percepción"
+        }
+        for concepto in perext["conceptosiapsep"].unique()
+        if concepto not in nocont[nocont["cuenta"].str.lower() == "no"]["concepto"].to_list()
+    ]
+    xindex, total_percep_extra = write_excel(ws, "Percepciones extraordinarias anuales", data, xindex, "Total Percepciones Extraordinarias")
+
+    # Deducciones de ley
+    total_deduc_ley = sum(
+        gended[(gended["conceptosiapsep"] == concepto) & (gended["qnaproc"] == lstqnaproc)]["importe"].sum()
+        for concepto in gended["conceptosiapsep"].unique()
+        if concepto in LEY_CONCEPTS
+    )
+
+    # Percepciones de sueldo
+    total_sueldo = sum(
+        perord[(perord["conceptosiapsep"] == concepto) & (perord["qnaproc"] == lstqnaproc)]["importe"].sum()
+        for concepto in perord["conceptosiapsep"].unique()
+        if concepto in SUELDO_CONCEPTS
+    )
+
+    # 6) Formato para pensiones alimenticias
+    if process_type == '1':
+        formula_result = calculate_amounts(total_percep_ord, total_percep_extra, total_deduc_ord, total_deduc_ley, total_sueldo, money_formula)
+        mount_to_discount = formula_result["amount"] * discount_percent
+
+        ws[f"B{xindex}"] = "Fórmula usada"
+        ws[f"C{xindex}"] = formula_result["formula_name"]
+        xindex += 1
+        ws[f"B{xindex}"] = "Descuento del " + str(discount_percent * 100) + "%"
+        ws[f"C{xindex}"] = mount_to_discount
+        xindex += 2
+
+        # Pagos retroactivos
+        ws[f"B{xindex}"] = "Periodo"
+        ws[f"C{xindex}"] = str(payment_period) + " Quincenas"
+        xindex += 1
+        ws[f"B{xindex}"] = "Consecutivo"
+        ws[f"C{xindex}"] = "Número de quincena"
+        ws[f"D{xindex}"] = "Monto"
+        xindex += 1
+
+        mount_per_period = mount_to_discount / payment_period
+        for i in range(payment_period):
+            lstqna_str = str(lstqnaproc)
+
+            year = lstqna_str[:4]
+            num_qna = lstqna_str[4:]
+
+            year = int(year)
+            num_qna = int(num_qna)
+
+            if (num_qna >= 24):
+                year += 1
+                num_qna = 0
+            
+            num_qna += 1
+
+            lstqnaproc = f"{year}{num_qna:02}"
+
+            ws[f"B{xindex}"] = i + 1
+            ws[f"C{xindex}"] = lstqnaproc
+            ws[f"D{xindex}"] = "{0:.2f}".format(mount_per_period)
+            xindex += 1
+
+    # 7) Guardar archivo Excel
+    dirpath = validate_dir(f"C:/caluladora/pensiones")
+    counters = len([file for file in os.listdir(dirpath) if f"{rfc}-" in file and ".xlsx" in file])
+    filename = f"{dirpath}/{rfc}-{dt.now().strftime('%d%m%Y')}_{counters + 1}.xlsx"
+
+    wb.save(filename=filename)
+    return filename
+
+
+def cli():
     # 1) Leer archivo
     process_type, discount_percent, money_formula, payment_period, file_name = prompt_user_input()
-    df = converter(file_name)
+    df = excel_2_dataframe(file_name)
 
     # 2) Leer percepciones y deducciones
     percep = load_excel_file("percepciones.xlsx")
@@ -250,7 +390,7 @@ def main():
 
     # 7) Guardar archivo Excel
     userpath = os.path.expanduser(os.getenv('USERPROFILE'))
-    dirpath = check_valid_dir(f"{userpath}/OneDrive - Secretaría de Educación de Guanajuato/tmp/Pensiones/{rfc}")
+    dirpath = validate_dir(f"{userpath}/OneDrive - Secretaría de Educación de Guanajuato/tmp/Pensiones/{rfc}")
     counters = len([file for file in os.listdir(dirpath) if f"{rfc}-" in file and ".xlsx" in file])
     filename = f"{dirpath}/{rfc}-{dt.now().strftime('%d%m%Y')}_{counters + 1}.xlsx"
 
@@ -262,7 +402,7 @@ def main():
 
 if __name__ == '__main__':
     try:
-        main()
+        cli()
     except Exception as e:
         print(f"Se ha producido un error: {e}")
     finally:
